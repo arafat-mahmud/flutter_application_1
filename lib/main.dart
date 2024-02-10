29ecc7f025ae07a9a5d0562445cda318bbe08a95
Arafat Mahmud <arafat.mahmud.2001@gmail.com>
2024-02-10 23:58:57 +0600

    Flutter

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text("This my first app for test"),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text('Click button'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
