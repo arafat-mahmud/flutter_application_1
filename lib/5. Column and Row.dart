35c7fcb2ec12b41dccc25c4734202ec11d69303e
Arafat Mahmud <arafat.mahmud.2001@gmail.com>
2024-02-12 13:18:35 +0600

    Column and Row

--- lib/5. Column and Row.dart
+++ lib/5. Column and Row.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
       home: SafeArea(
         child: Scaffold(
           appBar: AppBar(
-            title: const Text("Column"),
+            title: const Text("Row"),
             elevation: 5,
             backgroundColor: const Color.fromARGB(255, 65, 166, 233),
             ),
