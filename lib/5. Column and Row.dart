0bc4ddf0d6c9ad3e515581753c3afec86ac6af48
Arafat Mahmud <arafat.mahmud.2001@gmail.com>
2024-02-12 13:18:06 +0600

    Column and Row

---
+++ lib/5. Column and Row.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+   const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SafeArea(
+        child: Scaffold(
+          appBar: AppBar(
+            title: const Text("Column"),
+            elevation: 5,
+            backgroundColor: const Color.fromARGB(255, 65, 166, 233),
+            ),
+            body: Row(
+              children: <Widget>[
+                Container(
+                height: 200,
+                width: 150,
+                color: Colors.deepOrange,
+              ),
+              const SizedBox(
+                width: 10,
+              ),
+              Container(
+                height: 200,
+                width: 150,
+                color: Colors.deepOrange,
+              ),
+              Container(
+                height: 200,
+                width: 150,
+                color: Colors.deepOrange,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
