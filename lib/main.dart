b2e42ddb8b58281483e33311c20417fb36b5a730
Arafat Mahmud <arafat.mahmud.2001@gmail.com>
2024-02-13 21:13:48 +0600

    Basic Structure Widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,18 @@
 //  Welcome to Flutter
+
+// Structure Widget
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(
+        
+      ),
+    );
+  }
+}
