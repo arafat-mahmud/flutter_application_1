0e2fbc2e6b489725dd130eb553c0152c8414ce0b
Arafat Mahmud <arafat.mahmud.2001@gmail.com>
2024-02-14 10:44:09 +0600

    test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -6,7 +6,6 @@
 // tree, read text, and verify that the values of widget properties are correct.
 
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/2.%20Appbar%20and%20Text%20widget%20in%20flutter.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 // ignore: unused_import
