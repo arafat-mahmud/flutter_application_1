a366be5fc2cdb6517104cfc9a99fd7fa8af3bd7c
Arafat Mahmud <arafat.mahmud.2001@gmail.com>
2024-06-14 14:00:07 +0600

    ok

--- lib/19. Gradient color in appbar , container.dart
+++ lib/19. Gradient color in appbar , container.dart
@@ -1,1 +1,37 @@
 // ignore_for_file: use_key_in_widget_constructors, prefer_const_constructors, prefer_const_literals_to_create_immutables
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Colors.pink, Colors.deepPurple]
+          )
+        ),
+      ))
+    );
+  }
+}
