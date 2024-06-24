cd1319c6dda357df8ee5fc0d6789383fe67aceb2
Arafat Mahmud <arafat.mahmud.2001@gmail.com>
2024-06-25 01:02:01 +0600

    Animated

--- lib/33. Animated_Crossfade.dart
+++ lib/33. Animated_Crossfade.dart
@@ -1,1 +1,66 @@
+// ignore_for_file: use_key_in_widget_constructors, prefer_const_constructors,
+import 'package:flutter/material.dart';
 
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  bool check = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            AnimatedCrossFade(
+              firstChild: Container(
+                height: 200,
+                color: Colors.red,
+                child: Center(
+                  child: ElevatedButton(
+                    onPressed: () {
+                      setState(() {
+                        check = false;
+                      });
+                    },
+                    child: Text('Click'),
+                  ),
+                ),
+              ),
+              secondChild: SizedBox(
+                height: 300,
+                child: Center(
+                  child: Text(
+                    "Hello",
+                    style: TextStyle(fontSize: 59),
+                  ),
+                ),
+              ),
+              crossFadeState:
+                  check ? CrossFadeState.showFirst : CrossFadeState.showSecond,
+              duration: Duration(seconds: 2),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
