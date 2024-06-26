3f276ef82adde52886c9699280b1920524fd0e12
Arafat Mahmud <arafat.mahmud.2001@gmail.com>
2024-06-27 00:11:48 +0600

    DateTime

--- lib/39. DateTime_picker.dart
+++ lib/39. DateTime_picker.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: use_key_in_widget_constructors, prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 void main() {
   runApp(MyApp());
@@ -22,21 +23,54 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  DateTime? _dateTime;
+  TimeOfDay? _timeOfDay;
+
+  getDate() async {
+    DateTime? date = await showDatePicker(
+      context: context,
+      initialDate: DateTime(DateTime.now().year),
+      firstDate: DateTime(DateTime.now().year - 20),
+      lastDate: DateTime(DateTime.now().year + 2),
+    );
+    if (date != null) {
+      TimeOfDay? time = await showTimePicker(
+        context: context,
+        initialTime: TimeOfDay.now(),
+      );
+      if (time != null) {
+        setState(() {
+          _dateTime = date;
+          _timeOfDay = time;
+        });
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          ElevatedButton(child: null, onPressed: () {
-
-          }),
-          ElevatedButton(child: null, onPressed: () {
-            
-          }),
-        ],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            ElevatedButton(
+              child: Text("Pick Date and Time"),
+              onPressed: () {
+                getDate();
+              },
+            ),
+            ElevatedButton(
+              child: Text(
+                _dateTime != null && _timeOfDay != null
+                    ? "${DateFormat('yyyy-MM-dd').format(_dateTime!)} ${_timeOfDay!.format(context)}"
+                    : "No Date and Time Selected",
+              ),
+              onPressed: () {},
+            ),
+          ],
+        ),
       ),
-    ));
+    );
   }
 }
