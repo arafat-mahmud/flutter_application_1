ddc0c2574c3e9300820bed007d9b65055a4f4da6
Arafat Mahmud <arafat.mahmud.2001@gmail.com>
2024-06-27 22:31:45 +0600

    Transform

---
+++ lib/40. Flutter_Transform_widget.dart
@@ -0,0 +1,44 @@
+// ignore_for_file: use_key_in_widget_constructors, prefer_const_constructors, prefer_const_literals_to_create_immutables
+
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Transform.translate(
+          offset: Offset(50, -100),
+          
+          child: Container(
+            height: 150,
+            width: 150,
+            color: const Color.fromARGB(255, 234, 167, 167),
+          ),
+        ),
+        
+      )
+    );
+  }
+}
