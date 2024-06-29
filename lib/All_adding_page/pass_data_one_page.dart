b5a0719d85c547be51cba266d0245833d97bc194
Arafat Mahmud <arafat.mahmud.2001@gmail.com>
2024-06-29 22:57:55 +0600

    // ignore: must_be_immutable

--- lib/All_adding_page/pass_data_one_page.dart
+++ lib/All_adding_page/pass_data_one_page.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: use_key_in_widget_constructors, prefer_const_constructors
 import 'package:flutter/material.dart';
 
+// ignore: must_be_immutable
 class PassDataOnePage extends StatelessWidget {
   String sanon;
   PassDataOnePage({required this.sanon});
