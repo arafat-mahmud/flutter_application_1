c7700ac9e68153c32c8358682f1417197243295d
Arafat Mahmud <arafat.mahmud.2001@gmail.com>
2024-06-29 20:40:04 +0600

    fix

---
+++ lib/43. NavigationRail.dart
@@ -0,0 +1,52 @@
+// ignore_for_file: use_key_in_widget_constructors, prefer_const_constructors, prefer_const_literals_to_create_immutables
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _selectedIndex = 0;
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: Scaffold(
+      body: Row(
+        children: <Widget>[
+          NavigationRail(destinations: [
+            NavigationRailDestination(
+                icon: Icon(
+                  Icons.wifi
+                  ), 
+                indicatorColor: Colors.white,
+                label: Text(
+                  'WiFi',
+                  style: TextStyle(color: Colors.white),
+                ),
+                selectedIcon: Icon(
+                  Icons.wifi,
+                  color: Colors.blue,
+    
+            ),
+          )], selectedIndex: _selectedIndex)
+        ],
+      ),
+    ));
+  }
+}
