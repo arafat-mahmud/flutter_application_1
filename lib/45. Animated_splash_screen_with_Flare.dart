d4e795b2c0b1041ab7dcd8488a19ca1d91d14ad1
Arafat Mahmud <arafat.mahmud.2001@gmail.com>
2024-06-30 10:47:04 +0600

    just info for working sites

---
+++ lib/45. Animated_splash_screen_with_Flare.dart
@@ -0,0 +1,17 @@
+//Animated splash screen with Flare for using animations site- 
+
+//====== Choose any animation ======//
+// https://rive.app/community/files/
+
+
+
+
+// === Setup pubspec.yaml ===//
+
+// dependencies:
+// flare_flutter: ^3.0.2
+
+
+// assets: 
+// -animation/animation file name
+
