5d4698d83a5a88af980d4f86478d529298c6430f
Arafat Mahmud <arafat.mahmud.2001@gmail.com>
2024-07-06 22:44:04 +0600

    done

---
+++ lib/49. Change_Flutter_App_Icon_And_App_Name.dart
@@ -0,0 +1,54 @@
+// pubspece.yaml
+
+// # 49. Change_Flutter_App_Icon_And_App_Name.dart
+
+// ======== For name change ======
+
+//   flutter_launcher_icons: ^0.13.1
+// flutter_icons:
+//   android: true
+//   ios: true
+//   image_path: "images/launcher_icons.png"
+
+// =========  For icon change ======
+
+// ignore_for_file: use_key_in_widget_constructors, prefer_const_constructors, prefer_const_literals_to_create_immutables
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text(
+            "Hi",
+            style: TextStyle(fontSize: 24),
+          )
+        ],
+      ),
+    ));
+  }
+}
