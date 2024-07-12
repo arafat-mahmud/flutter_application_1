9c89a2658c857b16142f8dcf37e109b2027cd0e2
Arafat Mahmud <arafat.mahmud.2001@gmail.com>
2024-07-12 22:02:43 +0600

    drawer

--- lib/51. Flutter_Drawer.dart
+++ lib/51. Flutter_Drawer.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: use_key_in_widget_constructors, prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/All_adding_page/d.dart';
 
 void main() {
   runApp(MyApp());
@@ -25,9 +26,12 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        
-      )
+      drawer: Drawer(),
+      appBar: AppBar(
+        title: Text("Flutter Drawer"),
+        centerTitle: true,
+        ),
+      
     );
   }
 }
