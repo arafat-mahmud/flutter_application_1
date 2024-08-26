aba8465bc37b04edda7288308a461ea8cc4b9db4
Arafat Mahmud <arafat.mahmud.2001@gmail.com>
2024-08-26 10:54:24 +0600

    Flutter- WebView

---
+++ lib/56. Responsive_Ui.dart
@@ -0,0 +1,44 @@
+// ignore_for_file: use_key_in_widget_constructors, prefer_const_constructors, prefer_const_literals_to_create_immutables
+
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        
+        body: Center(
+          child: AspectRatio(
+            aspectRatio: 2,
+            child: Container(
+              color: Colors.blue,
+            ),
+          ),
+        ),
+        
+      )
+    );
+  }
+}
